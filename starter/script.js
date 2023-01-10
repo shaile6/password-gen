feb1f582a3c1bbcf23a3a25ee3384ad9612f5508
[shaile6] <[sara.haile@live.com]>
2023-01-10 00:10:55 +0000

    new code

---
+++ starter/script.js
@@ -0,0 +1,155 @@
+// Array of special characters to be included in password
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+
+// Array of numeric characters to be included in password
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+
+
+
+
+// Function to prompt user for password options
+function getPasswordOptions() {
+      
+}
+
+// Function for getting a random element from an array
+function getRandom(arr) {
+
+}
+
+// Function to generate password with user input
+
+function generatePassword() {
+  var passwordLength = prompt("How many characters would you like your password to contain?")
+
+  if (passwordLength < 10 || passwordLength > 64) {
+    alert("Error: Password length must be at least 10 characters and no more than 64.");
+        return;
+    } else{
+            var numericCharacters = confirm("Include numbers in the password?");
+            var specialCharacters = confirm("Include special characters in the password?");
+            var lowerCasedCharacters = confirm("Include lowercase characters in the password?");
+            var upperCasedCharacters = confirm("Include uppercase characters in the password?");
+          }
+
+
+  
+
+  
+  
+
+}
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate');
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector('#password');
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
